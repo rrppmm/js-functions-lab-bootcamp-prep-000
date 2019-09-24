124a973a52273e1f0de36df8e3bb0cd6df2db7a0
Paul M <paulmcadory@gmail.com>
2019-09-24 22:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function happyHolidays (string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo (string) {
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo (string) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown (string) {
+  return `It's ${days} days until ${holiday}!`
+}
